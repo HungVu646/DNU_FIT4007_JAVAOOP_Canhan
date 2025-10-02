f4d13379ce76a1c058346db066ccdde54717fa76
HungVu646 <vkhung20062006@example.com>
2025-10-02 21:12:28 +0700

    update part4 Ex11NaturalDivisors

---
+++ src/part4/Loops/Ex11NaturalDivisors.java
@@ -0,0 +1,16 @@
+package part4.loops;
+
+import java.util.Scanner;
+public class Ex11NaturalDivisors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số nguyên dương N: ");
+        int N = sc.nextInt();
+        System.out.println("Các ước của " + N + " là:");
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
